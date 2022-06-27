e0e13e4a08b4ddd94ef75936279cc62f45a994ae
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-27 11:38:56 +0000

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -17,11 +17,12 @@ app.get('/', function(req, res) {
   res.sendFile(homepage);
 });
 
-
-
-
-
-
+app.get('/json',function(req,res){
+  if(process.env.MESSAGE_STYLE === "uppercase")
+    res.json({"message": "HELLO JSON"});
+  else
+  res.json({"message": "Hello json"});
+});
 
 
 
