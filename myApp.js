19a13f4a76d7b31afa02729c860f2dad4cd87673
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-27 09:55:36 +0000

    function(req, res) Hello World

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+
+//console.log("Hello World")
+
+var str = "Hello World"
+
+app.get('/', function(req, res) {
+  res.send(str);
+});
 
 
 
