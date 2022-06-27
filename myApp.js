d35a4a13d577954d17d1d03aaa6a6ecd6bd4164c
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-27 11:08:59 +0000

    app.use("/public", assets)

--- myApp.js
+++ myApp.js
@@ -6,16 +6,16 @@ let app = express();
 
 //var str = "Hello Express"
 
+
+let assets = express.static(__dirname + "/public");
+app.use("/public", assets);
+
+
 var homepage = __dirname + "/views/index.html";
 app.get('/', function(req, res) {
   //res.send(str + "\n");
   res.sendFile(homepage);
 });
-
-let assets = express.static(__dirname + "/public/style.css");
-//app.use(assets);
-app.use(assets, middlewareFunction)
-
 
 
 
