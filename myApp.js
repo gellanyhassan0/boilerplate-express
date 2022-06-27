addd45068edca9312077245c689010cd4891ab99
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-27 10:37:13 +0000

    <link rel="stylesheet" href="/style.css">

--- myApp.js
+++ myApp.js
@@ -7,11 +7,14 @@ let app = express();
 //var str = "Hello Express"
 
 var homepage = __dirname + "/views/index.html";
-
 app.get('/', function(req, res) {
   //res.send(str + "\n");
   res.sendFile(homepage);
 });
+
+let assets = express.static(__dirname + "/public/style.css");
+//app.use(assets);
+app.use(assets, middlewareFunction)
 
 
 
